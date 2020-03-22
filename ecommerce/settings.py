0f6d7ddf564183b09bca6550ed232eeca2e6655f
Philipp Kundratitz <philippkundratitz@MacBook-Pro-Philipp.local>
2020-03-22 11:56:32 +0100

    Facebook login

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
     'crispy_forms',
     'django_countries'
 ]
@@ -94,6 +95,31 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+SOCIALACCOUNT_PROVIDERS = \
+    {'facebook':
+       {'METHOD': 'oauth2',
+        'SCOPE': ['email','public_profile', 'user_friends'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+            'first_name',
+            'last_name',
+            'verified',
+            'locale',
+            'timezone',
+            'link',
+            'gender',
+            'updated_time'],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': lambda request: 'kr_KR',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.4'}}
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1003273176733826' # APP ID
+SOCIAL_AUTH_FACEBOOK_SECRET = 'e8fe59b7c8660077eaed324ad86cd112' # secret key
+
 ACCOUNT_FORMS = {
 'signup': 'core.forms.CustomSignupForm',
 }
@@ -105,7 +131,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-SITE_ID = 1
+SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 
 # crispy forms
